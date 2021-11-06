9da46856b86bbfb9502cffcccbe1b9b6431b9830
AmcAndrew <90068691+AmcAndrew@users.noreply.github.com>
2021-11-06 13:04:34 -0700

    Update moveWithRays.cs

--- Assets/Scritps/moveWithRays.cs
+++ Assets/Scritps/moveWithRays.cs
@@ -1,1 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
+public class MainPlayer : MonoBehaviour {  
+  void Start () {
+    
+  }
+    
+  void Update () {
+    Ray ray = new Ray(transform.position, transform.forward);
+    RaycastHit hitInfo;
+  
+    if (Physics.Raycast(ray, out hitInfo)) {
+        //what happens when intersected
+    }else{
+        //what happens when not intersected
+     }
+  }
+}
