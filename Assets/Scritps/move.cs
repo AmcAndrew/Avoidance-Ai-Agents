fa8739adeb6b66b042c46b04093145d2231d865e
AmcAndrew <90068691+AmcAndrew@users.noreply.github.com>
2021-11-06 12:52:43 -0700

    Update move.cs

--- Assets/Scritps/move.cs
+++ Assets/Scritps/move.cs
@@ -1,1 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
+public class MainPlayer : MonoBehaviour {
+  public transform obsticalDetector;
+  public layerMask avoid;
+  
+  private bool obstical;
+  
+  void Start () {
+    
+  }
+    
+  void Update () {
+    
+  }
+}
